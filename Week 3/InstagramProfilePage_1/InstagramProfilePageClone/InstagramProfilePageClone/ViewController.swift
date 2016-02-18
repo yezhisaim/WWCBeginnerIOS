f7915ed1be6b9c7b2665ff20dba23f73beed2e50
Yezhisai Murugesan <yezhisaim@gmail.com>
2016-02-17 21:57:04 -0800

    Updating Step 1 with Header UIView for Profile Page
    - Flow of Steps
    1. Starter project with Header UIView
    2. Container Views for Images controlled by UISegmentedControl
    3. Cocoapods Integration
    4. Instagram setup
    5. Code to put it all together

---
+++ Week 3/InstagramProfilePage_1/InstagramProfilePageClone/InstagramProfilePageClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramProfilePageClone
+//
+//  Created by Yezhisai Murugesan on 2/17/16.
+//  Copyright © 2016 Isai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
