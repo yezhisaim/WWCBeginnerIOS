e4e322443a89a529dce27d8935e8ba624223dd06
Yezhisai Murugesan <yezhisaim@gmail.com>
2016-02-17 21:58:26 -0800

    Cocoapods Integration
    - Flow of Steps
    1. Starter project with Header UIView
    2. Container Views for Images controlled by UISegmentedControl
    3. Cocoapods Integration
    4. Instagram setup
    5. Code to put it all together

---
+++ Week 3/InstagramProfilePage_3/InstagramProfilePageClone/ProfilePageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfilePageTableViewCell.swift
+//  InstagramProfilePageClone
+//
+//  Created by Yezhisai Murugesan on 2/17/16.
+//  Copyright © 2016 Isai. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
